6ff59729a8f714f2813537ece92239896d63c084
Brian <brianrlam@gmail.com>
2023-05-26 12:54:21 -0400

    Updates

---
+++ precache-manifest.1cf90e3b4f770504649727ccd1461a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9849d1bc84df7a78d3e5",
+    "url": "/videogamerdle/static/css/main.ac7d22e0.chunk.css"
+  },
+  {
+    "revision": "9849d1bc84df7a78d3e5",
+    "url": "/videogamerdle/static/js/main.9849d1bc.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/videogamerdle/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "cddc92c48fb4db0237a8",
+    "url": "/videogamerdle/static/js/2.cddc92c4.chunk.js"
+  },
+  {
+    "revision": "393b67057eef0beaf50d",
+    "url": "/videogamerdle/static/js/runtime~main.393b6705.js"
+  },
+  {
+    "revision": "8446d08a11d869c73989e02a761bb4ed",
+    "url": "/videogamerdle/index.html"
+  }
+];
