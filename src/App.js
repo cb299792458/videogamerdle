22a4433d6e476d8ca92a78fa9d2b9fcfac352320
Brian <brianrlam@gmail.com>
2023-05-26 12:31:36 -0400

    finish tech demo

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import Board from './components/Board/Board';
 
 const example = {
   grid: [
-      ['Super Mario Bros. 2','Super Mario Odyssey','Super Mario World','Super Mario Galaxy'],
-      ['Final Fantasy III','Final Fantasy VII','Final Fantasy X','Final Fantasy VIII'],
-      ['The Legend of Zelda: Ocarina of Time',"The Legend of Zelda: Link's Awakening",'The Legend of Zelda: A Link to the Past','The Legend of Zelda: Tears of the Kingdom'],
-      ['Pokémon Blue','Pokémon Stadium','Pokémon Legends: Arceus','Pokémon GO'],
-  ]
+      ['Overwatch','Street Fighter II: The World Warrior','Super Mario Odyssey','The Legend of Zelda: A Link to the Past'],
+      ['Asteroids','The Legend of Zelda: Breath of the Wild','Super Mario Galaxy','World of Warcraft'],
+      ['Super Mario World', 'Diablo', 'Frogger','The Legend of Zelda: Tears of the Kingdom'],
+      ['The Legend of Zelda: A Link Between Worlds','Ms. Pac-Man','Starcraft','Super Mario 64'],
+  ],
+  answers: {
+    purple: ['Mario Series', 'Super Mario Galaxy', 'Super Mario Odyssey', 'Super Mario World', 'Super Mario 64'],
+    blue: ['Zelda Series', 'The Legend of Zelda: Breath of the Wild', 'The Legend of Zelda: Tears of the Kingdom', 'The Legend of Zelda: A Link to the Past', 'The Legend of Zelda: A Link Between Worlds'],
+    red: ['Arcade Games', 'Asteroids', 'Ms. Pac-Man', 'Street Fighter II: The World Warrior', 'Frogger'],
+    orange: ['Blizzard Games', 'Diablo', 'Starcraft', 'Overwatch', 'World of Warcraft'],
+    green: ['"World" Titles', 'World of Warcraft', 'Super Mario World', 'Street Fighter II: The World Warrior', 'The Legend of Zelda: A Link Between Worlds'],
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Video Game -rdle
+      <header className="App-header">Video Game-erdle
       </header>
-      <Board problem={example}/>
+      <Board puzzle={example}/>
     </div>
   );
 }
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,18 +3,25 @@ import React from 'react'
 import {useState} from 'react'
 
 function Board(props){
-    // console.log('Rerendered')
+    console.log('Rendering')
     const [swaps, setSwaps] = useState(0)
-    let board = props.problem;
-    let grid = board.grid;
-    
+    const puzzle = props.puzzle;
+    const grid = puzzle.grid;
+    const answers = puzzle.answers
+
     let origin = null;
-    
+
+    const colors = new Array(4).fill().map(()=>new Array(4).fill(null));
+    for(let r=0;r<4;r++){
+        for(let c=0;c<4;c++){
+            colors[r][c] = useState('')
+        }
+    }
+    const palette = ['purple','blue','red','orange','green']
+
     function selectOrigin(e){
         e.preventDefault();
-        // console.log(e.target.id)
         origin = JSON.parse('['+e.target.id+']')
-        // console.log(origin)
     }
     
     function selectDestination(e){
@@ -30,8 +37,45 @@ function Board(props){
         } else {
             [grid[orig[0]][orig[1]],grid[dest[0]][dest[1]]] = [grid[dest[0]][dest[1]],grid[orig[0]][orig[1]]];
 
+            check();
             setSwaps(swaps+1)
             origin=null;
+        }
+    }
+
+    function check(){
+
+        // reset colors
+        for(let r=0;r<4;r++){
+            for(let c=0;c<4;c++){
+                colors[r][c][1]('')
+            }
+        }
+
+        for(let color of palette){
+            // horizontal
+            for(let r=0;r<4;r++){
+                let count=0;
+                for(let c=0;c<4;c++){
+                    if(answers[color].includes(grid[r][c])) count++;
+                }
+                if(count===4){
+                    // console.log('MATCH: ', answer[0])
+                    for(let c=0;c<4;c++) colors[r][c][1](color)
+                }
+            }
+
+            // vertical
+            for(let c=0;c<4;c++){
+                let count=0;
+                for(let r=0;r<4;r++){
+                    if(answers[color].includes(grid[r][c])) count++;
+                }
+                if(count===4){
+                    // console.log('MATCH: ', answer[0])
+                    for(let r=0;r<4;r++) colors[r][c][1](color)
+                }
+            }
         }
     }
 
@@ -41,7 +85,7 @@ function Board(props){
                 {grid.map( (line,r) => {
                     return <div id='row' key={r}>
                         {line.map( (tile,c) => {
-                            return <div className='tile' key={c} id={[r,c]} onMouseDown={selectOrigin} onMouseUp={selectDestination}>
+                            return <div className='tile' key={c} id={[r,c]} style={colors[r][c][0] ? {backgroundColor: colors[r][c][0], color: 'white'} : {}} onMouseDown={selectOrigin} onMouseUp={selectDestination}>
                                 {grid[r][c]}
                             </div>
                         })}
