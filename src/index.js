ba3a37ba29f0f9ed66ca067aa75b539141d3d945
Brian <brianrlam@gmail.com>
2023-06-14 20:28:58 -0400

    revert homepage

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom/dist';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
