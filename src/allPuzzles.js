542b310583f80b8a7599ba09236a466841795fb5
Brian <brianrlam@gmail.com>
2023-06-15 14:51:54 -0400

    fix useState error

--- src/allPuzzles.js
+++ src/allPuzzles.js
@@ -2,9 +2,9 @@ const allPuzzles = {
     1: {
         red: ['Mario Series', 'Super Mario Galaxy', 'Super Mario Odyssey', 'Super Mario World', 'Super Mario 64'],
         blue: ['Zelda Series', 'The Legend of Zelda: Breath of the Wild', 'The Legend of Zelda: Tears of the Kingdom', 'The Legend of Zelda: A Link to the Past', 'The Legend of Zelda: A Link Between Worlds'],
-        pink: ['Arcade Games', 'Asteroids', 'Ms. Pac-Man', 'Street Fighter II: The World Warrior', 'Frogger'],
-        green: ['Blizzard Games', 'Diablo', 'Starcraft', 'Overwatch', 'World of Warcraft'],
-        purple: ['"World" Titles', 'World of Warcraft', 'Super Mario World', 'Street Fighter II: The World Warrior', 'The Legend of Zelda: A Link Between Worlds'],
+        purple: ['Arcade Games', 'Asteroids', 'Ms. Pac-Man', 'Street Fighter II: The World Warrior', 'Frogger'],
+        orange: ['Blizzard Games', 'Diablo', 'Starcraft', 'Overwatch', 'World of Warcraft'],
+        green: ['"World" Titles', 'World of Warcraft', 'Super Mario World', 'Street Fighter II: The World Warrior', 'The Legend of Zelda: A Link Between Worlds'],
     },
     2: {
         red: ['Mario Series', 'Super Mario Galaxy', 'Super Mario Odyssey', 'Super Mario World', 'Super Mario 64'],
--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,39 +1,35 @@
 import './Board.css';
 import React from 'react';
 import {useState} from 'react';
+import _ from 'lodash';
 
 function Board(props){
     // console.log('Rendering')
-    const [swaps, setSwaps] = useState(0)
-
+    
     const answers = props.answers;
-    const colors = Object.keys(answers);
-    const answerStatus = {};
-    for(let color of colors) answerStatus[color]=useState(false);
     const grid=props.grid;
     
+    const colors = Object.keys(answers);
+    const answerStatus = {};
+    for(let color of colors) answerStatus[color]=false;
+    const [answerState,setAnswerState] = useState(answerStatus);
+    
+    const [swaps, setSwaps] = useState(0)
     const [origin,setOrigin] = useState(null)
     
-    const styles = new Array(4).fill().map(()=>new Array(4).fill(null));
-    const highlights = new Array(4).fill().map(()=>new Array(4).fill(false));
-    for(let r=0;r<4;r++){
-        for(let c=0;c<4;c++){
-            styles[r][c] = useState('')
-            highlights[r][c]=useState(false)
-        }
-    }
+    const defaultStyles = new Array(4).fill().map(()=>new Array(4).fill(null));
+    const defaultHighlights = new Array(4).fill().map(()=>new Array(4).fill(false));
+    const [styles,setStyles] = useState(defaultStyles);
+    const [highlights,setHighlights] = useState(defaultHighlights);
     
+    // moving tiles
     function selectOrigin(e){
-        e.preventDefault();
         setOrigin(JSON.parse('['+e.target.id+']'));
     }
-    
     function selectDestination(e){
-        e.preventDefault();
         const destination = JSON.parse('['+e.target.id+']')
         swap(origin,destination);
     }
-    
     function swap(orig,dest){
         if(!orig || (orig[0]===dest[0] && orig[1]===dest[1])){
             // do nothing
@@ -43,19 +39,15 @@ function Board(props){
             check(grid);
             setSwaps(swaps+1)
         }
-        // console.log(origin)
         setOrigin(null);
     }
     
     function check(grid){        
-        // reset colors
-        for(let r=0;r<4;r++){
-            for(let c=0;c<4;c++){
-                styles[r][c][1]('')
-                highlights[r][c][1](false)
-            }
-        }
-        for(let color of colors) answerStatus[color][1](false);
+
+        // deepcopy states so they can be updated
+        const stylesCopy = _.cloneDeep(defaultStyles);
+        const highlightsCopy = _.cloneDeep(defaultHighlights);
+        const answerStateCopy = _.cloneDeep(answerStatus)
         
         for(let color of colors){
             // horizontal
@@ -66,12 +58,12 @@ function Board(props){
                     if(answers[color].includes(grid[r][c])) count++;
                 }
                 if(count===4){
-                    for(let c=0;c<4;c++) styles[r][c][1](color);
-                    answerStatus[color][1](true);
+                    for(let c=0;c<4;c++) stylesCopy[r][c]=color;
+                    answerStateCopy[color]=true;
                 }
                 if(count===3){
                     for(let c=0;c<4;c++){
-                        if(answers[color].includes(grid[r][c])) highlights[r][c][1](true)
+                        if(answers[color].includes(grid[r][c])) highlightsCopy[r][c] = true;
                     }
                 }
             }
@@ -85,45 +77,39 @@ function Board(props){
                 }
                 if(count===4){
                     for(let r=0;r<4;r++){ 
-                        styles[r][c][1](color);
-                        answerStatus[color][1](true);
+                        stylesCopy[r][c]=color;
+                        answerStateCopy[color]=true;
                     }
                 }
                 if(count===3){
                     for(let r=0;r<4;r++){
-                        if(answers[color].includes(grid[r][c])) highlights[r][c][1](true)
+                        if(answers[color].includes(grid[r][c])) highlightsCopy[r][c] = true;
                     }
                 }
             }
+            setHighlights(highlightsCopy);
+            setStyles(stylesCopy);
+            setAnswerState(answerStateCopy);
         }
-
-        // return {almost: almost, finished: finished}
     }
     
-    // // dragging
-    // const [position, setPosition] = useState({ x: 0, y: 0 })
-    // const handleDrag = (e) => {
-    //     e.preventDefault();
-    //     // console.log(e)
-    //     let data = {x: e.clientX,y: e.clientY}
-    //     setPosition({ x: data.x, y: data.y }) 
-    //     console.log(data.x,data.y)   
-    // };
-
-
+    function allowDrop(ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+    }
+    
     return(
         <>
-            <div id='board' >
+            <div id='board'>
                 {grid.map( (line,r) => {
                     return <div id='row' key={r} >
                         {line.map( (_,c) => {
-                            return <div className={`tile ${styles[r][c][0]} ${highlights[r][c][0] ? 'highlight' : ''}`} key={c} id={[r,c]}
-                            onPointerDown={selectOrigin}
-                            onPointerUp={selectDestination}
-                            // draggable="true" 
-                            // onDrag={handleDrag}
-                            // onDragStart={selectOrigin}
-                            style={ origin && origin[0]===r && origin[1]===c ? {transform: `scale(1.2)`, boxShadow: 'black 0px 5px 10px'} : {}}>
+                            return <div className={`tile ${styles[r][c]} ${highlights[r][c] ? 'highlight' : ''}`} key={c} id={[r,c]}
+                            draggable='true'
+                            onDragOver={allowDrop}
+                            onDragStart={selectOrigin}
+                            onDrop={selectDestination}
+                            style={ origin && origin[0]===r && origin[1]===c ? {transform: `scale(1.2)`, boxShadow: 'black 0px 2px 8px', backgroundColor: 'yellow'} : {}}>
                                 {grid[r][c]}
                             </div>
                         })}
@@ -136,8 +122,8 @@ function Board(props){
                     <span>Swaps: {swaps}</span>
                 </div>
                 <ol>
-                    {Object.values(answerStatus).map(function(bool,i){
-                        if(!bool[0]) return <li key={i}>?????</li>
+                    {Object.values(answerState).map(function(bool,i){
+                        if(!bool) return <li key={i}>?????</li>
                         else return <li className={colors[i]} key={i}>{Object.values(answers)[i][0]}</li>
                     })}
                 </ol>
